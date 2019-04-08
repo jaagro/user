4c4436ddf756f968c434b23fc920db384ba71e5a
baiyiran <1169172027@qq.com>
2019-04-08 10:15:30 +0800

    1

--- user-biz/src/main/java/com/jaagro/user/biz/service/impl/CustomerUserServiceImpl.java
+++ user-biz/src/main/java/com/jaagro/user/biz/service/impl/CustomerUserServiceImpl.java
@@ -71,7 +71,7 @@ public class CustomerUserServiceImpl implements CustomerUserService {
     /**
      * 创建customerUser
      *
-     * @param userDto
+     * @param userDtoList
      * @return
      */
     @Override
